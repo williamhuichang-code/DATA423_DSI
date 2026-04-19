c02680e0c4a2dea6e3d1021742ab67d4a0a7fa34
WilliamTheGreat <william.hui.chang@gmail.com>
2026-04-19 19:29:18 +1200

    submit ready

--- my_shiny_app/DSI_A2_app/my_modularised_framework/global.R
+++ my_shiny_app/DSI_A2_app/my_modularised_framework/global.R
@@ -23,7 +23,7 @@ library(ggrepel)      # label repelling in plots
 ##### change global configs when needed
 
 # file of interest
-FILE_OF_INTEREST <- "a2_14_accepted.csv"
+FILE_OF_INTEREST <- "Ass2Data.csv"
 
 # explicit data folder
 DATA_WD <- "."
