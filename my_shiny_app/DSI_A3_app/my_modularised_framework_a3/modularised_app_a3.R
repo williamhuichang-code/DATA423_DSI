7acea4c2756010e4adac19ee189d51868fa8f906
WilliamTheGreat <william.hui.chang@gmail.com>
2026-05-16 17:05:40 +1200

    experiments going so well

--- my_shiny_app/DSI_A3_app/my_modularised_framework_a3/modularised_app_a3.R
+++ my_shiny_app/DSI_A3_app/my_modularised_framework_a3/modularised_app_a3.R
@@ -91,7 +91,7 @@ task_specific_roles <- list(
 SPLIT_SEED   <- 199
 MODEL_SEED   <- 673
 AUTO_SPLIT   <- TRUE
-SPLIT_RATIO  <- 0.8
+SPLIT_RATIO  <- 0.3
 
 # Available Methods explorer — tags to exclude from the table/map by default,
 # and tags to highlight as literature-informed choices.
@@ -110,14 +110,14 @@ av_highlight_tags <- c(
   "Multivariate Adaptive Regression Splines"
 )
 
-# A3_omit_ids <- c(
-#   "tid-57748", "tid-57237", "tid-57537", "tid-57651",
-#   "tid-57689", "tid-57787", "tid-57761", "tid-57431",
-#   "tid-57479", "tid-57487", "tid-57600", "tid-57732",
-#   "tid-57739", "tid-57808", "tid-57845", "tid-57859",
-#   "tid-57921", "tid-57928", "tid-58050", "tid-58055",
-#   "tid-57470", "tid-57569", "tid-57580", "tid-57899"
-# )
+A3_omit_ids <- c(
+  "tid-57748", "tid-57237", "tid-57537", "tid-57651",
+  "tid-57689", "tid-57787", "tid-57761", "tid-57431",
+  "tid-57479", "tid-57487", "tid-57600", "tid-57732",
+  "tid-57739", "tid-57808", "tid-57845", "tid-57859",
+  "tid-57921", "tid-57928", "tid-58050", "tid-58055",
+  "tid-57470", "tid-57569", "tid-57580", "tid-57899"
+)
 
 
 # ── FILE LOADING LOGIC ───────────────────────────────────────────────────────
